25f30b35d52ed5b792077d7897c2ac496efc2f4e
Derya Antonelli <derya.antonelli@gmail.com>
2021-06-23 12:08:35 +0100

    Start server

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 const express = require("express");
 const app = express();
 const port = 3001;
+
+const persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-532425",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendick",
+    number: "39-23-2837383",
+  },
+];
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.listen(port, () => {
+    console.log(`Listening at ${port}`);
+})
