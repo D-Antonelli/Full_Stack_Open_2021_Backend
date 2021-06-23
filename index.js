a0f7d08d4726bcfef55b303dfad24de59d61183c
Derya Antonelli <derya.antonelli@gmail.com>
2021-06-23 14:26:03 +0100

    step 3

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response, json } = require("express");
 const express = require("express");
 const app = express();
 const port = 3001;
@@ -29,6 +30,18 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/info", (request, response) => {
+  const info = `<p>Phonebook has info for ${persons.length} people</p>`;
+  const date = `<p>${new Date()}</p>`;
+  response.send(`${info}${date}`);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((p) => p.id === id);
+  person ? response.json(person) : response.status(404).end();
+});
+
 app.listen(port, () => {
-    console.log(`Listening at ${port}`);
-})
+  console.log(`Listening at ${port}`);
+});
