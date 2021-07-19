26a861c2b90ff73fee701b817635a517be17e653
Derya Antonelli <derya.antonelli@gmail.com>
2021-07-19 12:01:14 +0100

    Add unique validator

--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const url = process.env.MONGODB_URI;
 
@@ -19,7 +20,7 @@ mongoose
   });
 
 const personSchema = new mongoose.Schema({
-  name: String,
+  name: { type: String, unique: true },
   number: String,
 });
 
@@ -30,5 +31,7 @@ personSchema.set("toJSON", {
     delete returnedObject.__v;
   },
 });
+
+personSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model("Person", personSchema);
